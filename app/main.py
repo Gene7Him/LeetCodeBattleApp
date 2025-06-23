fab98b913d99ea0b573a7414c98d8b4441334ebd
Eugene Faison <155666233+Gene7Him@users.noreply.github.com>
2025-06-22 19:19:42 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 #from sqlalchemy.ext.asyncio import AsyncSession
-from .database import models, schemas, database
+#from .database import models, schemas, database
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
